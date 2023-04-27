1c2276155393f322cba67fae36cbaf4101fca359
jz725 <118290531+jz725@users.noreply.github.com>
2023-04-26 21:38:52 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,22 +8,44 @@
 
 using namespace std;
 
-void createAccount();
-void login();
+void createAccount(vector<user>& users);
+void login(vector<user>& users);
 
 int main()
 {
-    cout << "Web name...." << endl;
-    //creating the menu
+    //creating a user class vector to store all the user class object
+    vector<user> users;
+
+    //parsing through text file and store everything in the vector
+    ifstream openfile;
+    openfile.open("accounts.txt");
+    string id, pw, fn, ln, em, ph;
+    while(openfile >> id >> pw >> fn >> ln >> em >>ph)
+    {
+        //make a user class and store all information
+        user temp(id, pw, fn, ln, em, ph);
+
+        //push the user class into the vector
+        users.push_back(temp);
+    }
+
+    //print web name
+    cout << "=========================================" << endl;
+    cout << "=========================================" << endl;
+    cout << "   GROUP EIGHTEEN VIDEO GAME STORE       " << endl;
+    cout << "=========================================" << endl;
+    cout << "=========================================" << endl;
+
+    //creating and printing main menu
     while(1)
     {
         string option;
-        cout << endl << "Menu:" << endl;
+        cout << endl << "MENU:" << endl;
         cout << "0. Exit" << endl;
         cout << "1. Login" << endl;
         cout << "2. Create Account" << endl;
 
-        cout << "Choose an option: ";
+        cout << "Choose an option: ";  //prompt user for an option
         cin >> option;
         cout << endl;
 
@@ -36,166 +58,149 @@ int main()
         //Login
         else if(option == "1")
         {
-            login();
+            /*user user;
+            user.login();*/
+            login(users);
         }
         //Create Account
         else if(option == "2")
         {
-            createAccount();
+            //user newuser;
+            // newuser.Register();
+            createAccount(users);
         }
         //Not an option
         else
         {
             cout << "Invalid Option. Please try again." << endl;
         }
+
+
     }
+
     return 0;
 }
 
-void createAccount()
+//create account function that utilize passed by reference to modify actual content
+void createAccount(vector<user>& users)
 {
-    string newUsername, checkUsername, newPassword, confirmPassword, firstName, lastName, address, phoneNum, email, creditcardNum;
-    int userSize;
-    int checkPassSize;
-    bool ON = true;
+    string newUsername, checkUserID, newPassword, confirmPassword, firstName, lastName, phoneNum, email;
+    int checkPassSize; //int variable to store new password length to validate
     bool oo = true;
+    bool valid = true; //Boolean to check if username is valid
+
     cout << "\n-----Create New Account-----\n";
 
-    //checking if username is taken or not
     while(oo)
     {
         string choice;
-        cout << "Do you want to start registering? (yes/no): ";
+        cout << "Do you want to start registering? (yes/no): "; //make sure user didn't choose the wrong menu option
         cin >> choice;
-        if (choice != "yes" && choice != "no" )
+        if(choice != "yes" && choice != "no") //if not yes and no
         {
             cout << "Invalid answer. Try again." << endl;
-            cout << "Do you want to start registering? (yes/no): ";
+            cout << "Do you want to start registeringg? (yes/no): ";
             cin >> choice;
         }
-        if(choice == "no")
+        if (choice == "no")
         {
-            main();
+            return; //break from function and go back to main
         }
-
-        cout << endl;
-
-        cout << "Username: ";
-        cin >> newUsername;
-
-        userSize = newUsername.length();
-        fstream users;
-        users.open("accounts.txt", ios::in);
-        if(ON && userSize >= 1 && users.is_open())
-        {
-            string line;
-            while(getline(users, line))
-            {
-                stringstream OnlyUsers(line);
-                OnlyUsers >> checkUsername;
-                if(newUsername == checkUsername)
-                {
-                    cout << "\n" << newUsername << " is already taken. Try another one.\n" << endl;
-                    ON = false;
-                }
-            }
-        }
-        users.close();
-        if(!ON)
-        {
-            ON = true;
-        }
-        else
-        {
-            oo = false;
-        }
-
+        oo = false;
     }
 
-    //Validating Password
-    while(1)
+    cout << endl;
+    //if 'yes', continue to registration process
+    cout << "Username: ";
+    cin >> newUsername;
+
+    //loop through vector<user> users and see if new username is taken already
+    for(unsigned int i = 0; i < users.size(); i++)
     {
-        cout << "Password: ";
-        cin >> newPassword;
-
-        checkPassSize = newPassword.length();
-        if(checkPassSize < 7)
+        if(users[i].getUserID() == newUsername)
         {
-            cout << endl << "Password is too weak, need to have a length of 7+" << endl << endl;
-
+            cout << "\n" << newUsername << " is already taken. Try another one.\n" << endl;
+            valid = false;
         }
-        else
+    }
+
+    //if username not taken
+    if(valid)
+    {
+        //validating password
+        while(1)
         {
-            cout << "Confirm password: ";
-            cin >> confirmPassword;
-            if(confirmPassword == newPassword)
+            cout << "Password: ";
+            cin >> newPassword;
+
+            checkPassSize = newPassword.length();
+            if(checkPassSize < 7)
             {
-                cout << "Enter firstname: ";
-                cin >> firstName;
-                cout << "Enter lastname: ";
-                cin >> lastName;
-                cout << "Enter email: ";
-                cin >> email;
-                cout << "Enter phone number: ";
-                cin >> phoneNum;
-
-                fstream users;
-                users.open("accounts.txt", ios::app);
-                if(users.is_open())
-                {
-                    users << newUsername << " " << newPassword << " " << firstName << " " << lastName << " " << email << " " << phoneNum << endl;
-                    users.close();
-                }
-                cout << "\n=================================\n";
-                cout <<"Account sucessfully created! \n";
-                cout << "==================================\n";
-                cout << endl;
-
-                main();
-
+                cout << endl << "Password is too weak, need to have a length of 7+" << endl << endl;
             }
             else
             {
-                cout << "\nPassword confirmation does not match. \n" << endl;
+                cout << "Confirm password: ";
+                cin >> confirmPassword;
+                if(confirmPassword == newPassword)
+                {
+                    cout << "Enter firstname: ";
+                    cin >> firstName;
+                    cout << "Enter lastname: ";
+                    cin >> lastName;
+                    cout << "Enter email: ";
+                    cin >> email;
+                    cout << "Enter phone number: ";
+                    cin >> phoneNum;
+                    user temp(newUsername, newPassword, firstName, lastName, email, phoneNum);
+                    users.push_back(temp);
+
+                    //write the contents in the file
+                    fstream users;
+                    users.open("accounts.txt", ios::app);
+                    if(users.is_open())
+                    {
+                        users << newUsername << " " << newPassword << " " << firstName << " " << lastName << " " << email << " " << phoneNum << endl;
+                        users.close();
+                    }
+                    cout << "\n=================================\n";
+                    cout <<"Account sucessfully created! \n";
+                    cout << "==================================\n";
+                    cout << endl;
+
+                    main();
+
+                }
+                else
+                {
+                    cout << "\nPassword confirmation does not match. \n" << endl;
+                }
             }
         }
-
-
+        
     }
+    
 
 }
 
-void login()
+void login(vector<user>& users)
 {
-    string choice;
-    cout << "Do you want to proceed with login process? (yes/no): ";
-    cin >> choice;
-    if (choice != "yes" && choice != "no" )
-    {
-        cout << "Invalid answer. Try again." << endl;
-        cout << "Do you want to proceed with login process? (yes/no): ";
-        cin >> choice;
-    }
-    if(choice == "no")
-    {
-        main();
-    }
     int exist;
-    string logUsername, logPassword, u, p;
+    string logUsername, logPassword;
     cout << "Enter the username: ";
     cin >> logUsername;
     cout << "Enter the password: ";
     cin >> logPassword;
-    ifstream user("accounts.txt");
 
-    while(user>>u>>p)
+    //loop through users vector and see if there is a matching user
+    for(unsigned int i = 0; i < users.size(); i++)
     {
-        if(u == logUsername && p == logPassword)
+        if(users[i].getUserID() == logUsername && users[i].getPassword() == logPassword)
         {
             exist = 1;
         }
     }
-    user.close();
+
     if(exist == 1)
     {
         cout << endl;
