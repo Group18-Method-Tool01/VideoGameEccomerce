c3a1cd51fd549dd7621609d1c222b6c5614b1d29
jz725 <118290531+jz725@users.noreply.github.com>
2023-04-28 22:25:21 -0500

    Update navigation.cpp

--- navigation.cpp
+++ navigation.cpp
@@ -34,6 +34,7 @@ void Navigation::mainmenu()
 
     m:
     //print web name
+    cout << endl;
     cout << "=========================================" << endl;
     cout << "=========================================" << endl;
     cout << "   GROUP EIGHTEEN VIDEO GAME STORE       " << endl;
@@ -42,9 +43,9 @@ void Navigation::mainmenu()
 
     int option;
     cout << endl << "MENU:" << endl;
-    cout << "1. Exit" << endl;
-    cout << "2. Login" << endl;
-    cout << "3. Create Account" << endl;
+    cout << "1. Login" << endl;
+    cout << "2. Create Account" << endl;
+    cout << "3. Exit" << endl;
 
     cout << "Choose an option: ";  //prompt user for an option
     cin >> option;
@@ -54,15 +55,17 @@ void Navigation::mainmenu()
     {
         case 1:
             {
-                exit(0);
+                login(users);
+                break;
             }
         case 2:
             {
-                login(users);
+                createAccount(users);
+                break;
             }
         case 3:
             {
-                createAccount(users);
+                exit(0);
             }
         default:
             {
@@ -117,9 +120,9 @@ void Navigation::login(vector<user>& users)
         cout << endl;
         cout << "Logging in...";
         cout << endl;
-        cout << "===================" << endl;
+        cout << "=========================" << endl;
         cout << "Welcome " << userFirst << endl;
-        cout << "===================" << endl;
+        cout << "=========================" << endl;
         afterLogin();
     }
     else
@@ -234,11 +237,30 @@ void Navigation::createAccount(vector<user>& users)
 
 void Navigation::afterLogin()
 {
+    //creating a user class vector to store all the user class object
+    vector<user> users;
+
+    //parsing through text file and store everything in the vector
+    ifstream openfile;
+    openfile.open("accounts.txt");
+    string id, pw, fn, ln, em, ph;
+    while(openfile >> id >> pw >> fn >> ln >> em >>ph)
+    {
+        //make a user class and store all information
+        user temp(id, pw, fn, ln, em, ph);
+
+        //push the user class into the vector
+        users.push_back(temp);
+    }
+    r:
     int choice;
+    cout << "=========================" << endl;
     cout << "|____1) Browse Game ____|" << endl;
     cout << "|____2) View Cart ______|" << endl;
     cout << "|____3) Manage Profile _|" << endl;
     cout << "|____4) Logout__________|" << endl;
+    cout << "=========================" << endl;
+    cout << endl;
 
     cout << "Please select a choice: ";
     cin >> choice;
@@ -247,25 +269,203 @@ void Navigation::afterLogin()
     {
         case 1:
             {
-                cout << "Browse Game" << endl;
+                browseGame();
+                break;
             }
         case 2:
             {
-                cout << "View Cart" << endl;
+                cout << "CODE NEEDED" << endl;
+                break;
+                //code needed for viewing cart item
             }
         case 3:
             {
-                cout << "Manage Profile" << endl;
+                manageProfile(users);
+                break;
+
             }
         case 4:
             {
                 mainmenu();
+                break;
             }
+        default:
+            {
+                cout << endl;
+                cout << "Invalid Choice. Please try again." << endl << endl;
+                goto r;
+            }
+
 
     }
 }
 
+void Navigation::browseGame()
+{
+    cout << "hello...."<< endl;
+    //CODE NEEDED for displaying all the game to the user;
+}
+
 void Navigation::manageProfile(vector<user>& users)
 {
+    int exist;
+    string ID, profileID, profilePW, profileFirst, profileLast, profileEmail, profilePhone;
+    b:
+    cout << "Please enter userID to proceed: ";
+    cin >> ID;
+    for(unsigned int i = 0; i < users.size(); i++)
+    {
+        if(users[i].getUserID() == ID)
+        {
+            r:
+            //creating each variable to store the information from user profile
+            profileID = users[i].getUserID();
+            profilePW = users[i].getPassword();
+            profileFirst = users[i].getFirst();
+            profileLast = users[i].getLast();
+            profileEmail = users[i].getEmail();
+            profilePhone = users[i].getPhone();
+            int choice;
+
+            cout << "======================================" << endl;
+            cout << "            Manage Profile            " << endl;
+            cout << "======================================" << endl;
+            cout << "|____1) Edit Account Information ____|" << endl;
+            cout << "|____2) View Order History___________|" << endl;
+            cout << "|____3) Delete Account_______________|" << endl;
+            cout << "======================================" << endl;
+            cout << endl;
+
+            cout << "Please select a choice: ";
+            cin >> choice;
+
+            switch(choice)
+            {
+                case 1:
+                    {   s:
+                        cout << "================================" << endl;
+                        cout << "         Manage Profile         " << endl;
+                        cout << "================================" << endl;
+                        cout << "UserID: " << profileID << endl;
+                        cout << "Password: *******" << endl;
+                        cout << "First Name: " << profileFirst << endl;
+                        cout << "Last Name: " << profileLast << endl;
+                        cout << "Email: " << profileEmail << endl;
+                        cout << "Phone#: " << profilePhone << endl;
+                        cout << "================================" << endl;
+
+                        int option;
+                        cout << endl;
+                        cout << "What would you like to change?" << endl;
+                        cout << "1. UserID" << endl;
+                        cout << "2. Password" << endl;
+                        cout << "3. First Name" << endl;
+                        cout << "4. Last Name" << endl;
+                        cout << "5. Email" << endl;
+                        cout << "6. Phone#" << endl;
+                        cout << endl;
+                        cout << "Select an option: ";
+                        cin >> option;
+                        switch(option)
+                        {
+                            case 1:
+                                {
+                                    bool valid = true;
+                                    string changeID;
+                                    cout << "Enter new userID: ";
+                                    cin >> changeID;
+                                    //loop through vector<user> users and see if new username is taken already
+                                    for(unsigned int i = 0; i < users.size(); i++)
+                                    {
+                                        if(users[i].getUserID() == changeID)
+                                        {
+                                            cout << "\n" << changeID << " is already taken. Try another one.\n" << endl;
+                                            valid = false;
+                                        }
+                                        //new userID not taken
+                                        if(valid)
+                                        {
+                                            cout << "UNFINISH CODING!!!" << endl;
+                                            //modify in vector and change in accounts.txt file
+                                        }
+                                    }
+
+                                }
+                            case 2:
+                                {
+
+                                    string changePW, confirmPW;
+                                    cout << "Enter new password: ";
+                                    cin >> changePW;
+                                    cout << "Confirm new password: ";
+                                    if(changePW == confirmPW)
+                                    {
+                                        cout << "UNFINISH CODING!!!!" << endl;
+                                        //modify password in vector and accounts.txt
+                                    }
+
+
+                               }
+                            case 3:
+                                {
+                                    cout << "UNFINISH CODING!!!" << endl;
+                                    //NOT FINISH
+                                }
+                            case 4:
+                                {
+                                    cout << "UNFINISH CODING!!!" << endl;
+                                    //NOT FINISH
+                                }
+                            case 5:
+                                {
+                                    cout << "UNFINISH CODING!!!" << endl;
+                                    //NOT FINISH
+                                }
+                            case 6:
+                                {
+                                    cout << "UNFINISH CODING!!!" << endl;
+                                    //NOT FINISH
+                                }
+                            default:
+                                {
+                                    cout << endl;
+                                    cout << "Invalid Choice. Please try again." << endl << endl;
+                                    goto s;
+                                }
+                        }
+
+                    }
+                 case 2:
+                     {
+                        cout << "CODE NEEDED" << endl;
+                        break;
+                        //CODE NEEDED for viewing order history
+                     }
+                 case 3:
+                     {
+                        cout << "CODE NEEDED" << endl;
+                        break;
+                        //CODE NEEDED for removing account and all its information from file;
+
+                     }
+
+                 default:
+                     {
+                        cout << endl;
+                        cout << "Invalid Choice. Please try again." << endl << endl;
+                        goto r;
+                     }
+
+
+            }
+
+        exist = 1;
+        }
+    }
+
+    cout << "UserID does not match. Please try again." << endl;
+    goto b;
+
+
 
 }
